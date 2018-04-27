13bab3c93d43451abe9ddecd8e6b6deba2e96908
Sreerag Mandakathil <mandakathil.s@husky.neu.edu>
2018-04-27 19:12:09 -0400

    final

--- dockerfile
+++ dockerfile
@@ -6,4 +6,11 @@ RUN apt-get install python3 python3-pip -y
 RUN pip3 install django
 RUN pip3 install PyPDF2
 RUN pip3 install luigi
+RUN pip3 install numpy
+RUN pip3 install pandas
+RUN pip3 install sklearn
+RUN pip3 install scipy
+RUN pip3 install nltk
+RUN pip3 install textblob
+RUN pip3 install boto3
 COPY mlbcsite/. /mlbcsite
--- mlbcsite/sunprocess.py
+++ mlbcsite/sunprocess.py
@@ -2,8 +2,10 @@ import subprocess
 import time
 import sys
 
+
+
 print(sys.argv)
-commands = ["luigid","python manage.py runserver 0.0.0.0:8000","python luigi_pipeline.py uploadmodeltos3 --workers 2 --akey "+ sys.argv[0] +" --skey "+ sys.argv[0]]
+commands = ["luigid","python3 manage.py runserver 0.0.0.0:8000","python3 luigi_pipeline.py uploadmodeltos3 --workers 2 --akey "+ sys.argv[0] +" --skey "+ sys.argv[0]]
 # commands = ["luigid"]
 
 subprocess.Popen(commands[0].split(" "))
